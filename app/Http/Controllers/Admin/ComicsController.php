3d1b048fe702f34a940b179125ce5ec13f54a4e2
Kejdi Kishta <155896977+KejdiKishta@users.noreply.github.com>
2024-06-17 09:02:18 +0200

    add: index page

--- app/Http/Controllers/Admin/ComicsController.php
+++ app/Http/Controllers/Admin/ComicsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Comic;
 use Illuminate\Http\Request;
 
 class ComicsController extends Controller
@@ -12,7 +13,8 @@ class ComicsController extends Controller
      */
     public function index()
     {
-        //
+        $comicsArray = Comic::all();
+        return view('comics.index', compact('comicsArray'));
     }
 
     /**
--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,7 +1,22 @@
-@extends('app')
+@extends('layouts.app')
 
 @section('content')
     <div class="container">
-        
+        <div class="row">
+            @foreach ($comicsArray as $item)        
+            <div class="card col-3 p-2 border-0">
+                <img src="{{ $item->thumb }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $item->title }}</h5>
+                    <small>
+                        {{ $item->series }} <br>
+                        type: {{ $item->type }}
+                    </small>
+                    <p class="card-text">{{ $item->description }}</p>
+                    <p>price: {{ $item->price }}</p>
+                </div>
+            </div>
+            @endforeach
+        </div>
     </div>
 @endsection
