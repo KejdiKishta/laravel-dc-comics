e9b374a3ff6878fac36182524adfcca4db06ff08
Kejdi Kishta <155896977+KejdiKishta@users.noreply.github.com>
2024-06-17 18:16:07 +0200

    add: delete

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -12,8 +12,13 @@
                     <div>
                         <small>{{ $item->series }}</small>
                     </div>
-                    <a href="{{ route('comics.show', ['comic' => $item->id]) }}">Details</a>
-                    <a href="{{ route('comics.edit', ['comic' => $item->id]) }}">Edit</a>
+                    <a href="{{ route('comics.show', ['comic' => $item->id]) }}"><button class="btn btn-primary">Details</button></a>
+                    <a href="{{ route('comics.edit', ['comic' => $item->id]) }}"><button class="btn btn-warning">Edit</button></a>
+                    <form action="{{ route('comics.destroy', ['comic' =>  $item->id]) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button class="btn btn-danger">Delete</button>
+                    </form>
                 </div>
             </div>
             @endforeach
